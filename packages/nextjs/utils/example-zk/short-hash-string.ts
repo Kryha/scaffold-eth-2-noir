61186eef843be012a2dd16cee5cb52c2a5e8d2d3
tim kryha <tim@kryha.io>
2023-08-25 15:53:38 +0200

    increase prefix length of shorten hash string function

--- packages/nextjs/utils/example-zk/short-hash-string.ts
+++ packages/nextjs/utils/example-zk/short-hash-string.ts
@@ -1,5 +1,5 @@
 export function shortenHashString(hash: string) {
-  const prefix = hash.substring(0, 2);
+  const prefix = hash.substring(0, 4);
   const suffix = hash.substring(hash.length - 4);
 
   return prefix + "..." + suffix;
